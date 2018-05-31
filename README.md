a9c00cb740767e87fffa9b068605286dde5b4727
dxcweb <dxcweb@qq.com>
2018-05-31 16:17:04 +0800

    no message

--- README.md
+++ README.md
@@ -1,2 +1,71 @@
 # dxc-upload
 截图后粘贴、拖拽和点击上传预览，图片压缩。
+## install
+
+```
+npm install dxc-upload --save
+```
+## Development
+
+```
+npm install
+npm start
+```
+## Example
+[http://github.dxcweb.com/dxc-upload/](http://github.dxcweb.com/dxc-upload/ "http://github.dxcweb.com/dxc-upload/")
+
+## API
+<table class="table table-bordered table-striped">
+ <thead>
+    <tr>
+      <th style="width: 100px;">Name</th>
+      <th style="width: 50px;">Type</th>
+      <th>Default</th>
+      <th>Description</th>
+    </tr>
+  </thead>
+<tbody>
+    <tr>
+      <td>paste</td>
+      <td>Bool</td>
+      <td>true</td>
+      <td>是否支持粘贴</td>
+    </tr>
+     <tr>
+      <td>drop</td>
+      <td>Bool</td>
+      <td>true</td>
+      <td>是否支持拖拽</td>
+    </tr>
+	<tr>
+      <td>domain</td>
+      <td>String</td>
+      <td>document</td>
+      <td>域 document或者self 指drop或paste在什么域上</td>
+    </tr>
+    <tr>
+      <td>multiple</td>
+      <td>Bool</td>
+      <td>false</td>
+      <td>input multiple 属性</td>
+    </tr>
+	<tr>
+      <td>accept</td>
+      <td>String</td>
+      <td>image/png,image/gif,image/jpeg</td>
+      <td>允许上传文件类型</td>
+    </tr>
+	<tr>
+      <td>onError</td>
+      <td>Function</td>
+      <td>null</td>
+      <td>类型错误时调用参数（msg）</td>
+    </tr>
+	<tr>
+      <td>onChange</td>
+      <td>Function</td>
+      <td>null</td>
+      <td>输入参数files</td>
+    </tr>
+</tbody>
+</table>
---
+++ src/Upload.js
@@ -0,0 +1,163 @@
+import React, { Component, PropTypes } from "react";
+import ReactDOM from "react-dom";
+import utils from "./utils";
+class Upload extends Component {
+  static defaultProps = {
+    paste: true, //是否支持粘贴
+    drop: true, //是否支持拖拽
+    domain: "document", //域 document或者self 指drop或paste在什么域上
+    accept: "image/png,image/gif,image/jpeg", //允许上传文件格式
+    multiple: false,
+    onError: msg => {
+      console.error(msg);
+    },
+    onChange: () => {}
+  };
+
+  componentDidMount() {
+    const { paste, drop, domain } = this.props;
+    let dom = null;
+    if (domain == "document") {
+      dom = document;
+    } else {
+      dom = this.refs.self;
+    }
+
+    if (drop) {
+      this.stopBrowserAction = e => {
+        e.stopPropagation();
+        e.preventDefault();
+      };
+      //阻止浏览器默行为。
+      document.addEventListener("dragover", this.stopBrowserAction);
+      document.addEventListener("drop", this.stopBrowserAction);
+      this.dropEvent = e => {
+        e.stopPropagation();
+        e.preventDefault();
+        this.drop(e);
+      };
+      //添加拖拽事件
+      dom.addEventListener("drop", this.dropEvent);
+    }
+    if (paste) {
+      this.pasteEvent = e => {
+        this.paste(e);
+      };
+      dom.addEventListener("paste", this.pasteEvent);
+    }
+  }
+
+  //组件移除前调用。
+  componentWillUnmount() {
+    const { paste, drop, domain } = this.props;
+    let dom = null;
+    if (domain == "document") {
+      dom = document;
+    } else {
+      dom = this.refs.self;
+    }
+    document.removeEventListener("dragover", this.stopBrowserAction);
+    document.removeEventListener("drop", this.stopBrowserAction);
+    dom.removeEventListener("drop", this.dropEvent);
+    dom.removeEventListener("paste", this.pasteEvent);
+  }
+
+  //过滤
+  filter(file) {
+    const { accept, onError } = this.props;
+    const index = accept.indexOf(file.type);
+    if (index < 0) {
+      return false;
+    }
+    return true;
+  }
+
+  //拖拽
+  drop(e) {
+    const fileList = event.dataTransfer.files;
+    if (!fileList || !fileList.length) return;
+    const files = [];
+    for (let i = 0, item; (item = fileList[i]); i++) {
+      if (this.filter(item)) {
+        files.push(item);
+      }
+    }
+    const { onChange, onError } = this.props;
+    if (files.length == 0) {
+      onError("文件类型错误!");
+      return false;
+    }
+    onChange(files);
+  }
+
+  //粘贴
+  paste(e) {
+    if (e.clipboardData && e.clipboardData.items && e.clipboardData.items.length) {
+      const files = [];
+      for (let i = 0, item; (item = e.clipboardData.items[i]); i++) {
+        if (this.filter(item)) {
+          files.push(item.getAsFile());
+        }
+      }
+      const { onChange, onError } = this.props;
+      if (files.length == 0) {
+        onError("文件类型错误!");
+        return false;
+      }
+      onChange(files);
+    }
+  }
+
+  onClick() {
+    this.refs.inputFile.click();
+  }
+
+  onInputChange(e) {
+    var file = e.target.files[0];
+    if (file == null) {
+      return false;
+    }
+    const files = [];
+    if (this.filter(file)) {
+      files.push(file);
+    }
+    const { onChange, onError } = this.props;
+    if (files.length <= 0) {
+      onError("文件类型错误!");
+      return false;
+    }
+    this.refs.inputFile.value = "";
+    onChange(files);
+  }
+
+  render() {
+    const {
+      children,
+      onClick,
+      paste,
+      drop,
+      domain,
+      accept,
+      onError,
+      onChange,
+      ...other
+    } = this.props;
+    return (
+      <div {...other} onClick={this.onClick.bind(this)} ref="self">
+        <input
+          accept={accept}
+          multiple="multiple"
+          onChange={this.onInputChange.bind(this)}
+          type="file"
+          ref="inputFile"
+          style={{ display: "none" }}
+        />
+        {children}
+      </div>
+    );
+  }
+}
+Upload.filesToDataURL = utils.filesToDataURL;
+Upload.compressImage = utils.compressImage;
+
+export default Upload;
